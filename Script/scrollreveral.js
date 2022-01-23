6d3290049d037634f880f68a16ce405178e28e5e
jeskarin36 <94501234+jeskarin36@users.noreply.github.com>
2022-01-23 09:35:43 -0400

    Update scrollreveral.js

--- Script/scrollreveral.js
+++ Script/scrollreveral.js
@@ -65,6 +65,42 @@ window.addEventListener('scroll', () => {
 })
 
 
+let array=["url('./img/photo-1565958011703-44f9829ba187.jpeg')","url('./img/photo-1562967914-608f82629710.jpeg')",
+  "url('./img/photo-1541832676-9b763b0239ab.jpeg')","url('./img/photo-1562036409-9dcc48472e29.jpeg')","url('./img/photo-1473093295043-cdd812d0e601.jpeg')"];
+
+
+ let bigimg=document.querySelector(".big-img");
+ bigimg.style.backgroundImage=array[0];
+ i=0;
+
+
+
+ seccionatras=document.querySelector(".seccion-menu-bloque-2-atras");
+ seccionsiguiente=document.querySelector(".seccion-menu-bloque-2-siguiente");
+
+ seccionatras.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(i>0 && i<=5){
+        i--;
+    }else{
+        i=5;
+    }
+    bigimg.style.backgroundImage=array[i];
+    
+ })
+
+ 
+ seccionsiguiente.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(i<5){
+        i++;
+    }else{
+        i=0;
+    }
+    bigimg.style.backgroundImage=array[i];
+
+  
+ })
 
 
     

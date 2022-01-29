0b1a856361633467ed1e02198cff0a50ef286e9a
jeskarin36 <94501234+jeskarin36@users.noreply.github.com>
2022-01-29 16:18:36 -0400

    Update articulos.js

--- Script/articulos.js
+++ Script/articulos.js
@@ -53,3 +53,44 @@ window.addEventListener('scroll', () => {
 
 
 
+
+
+  let array=["url('./img/photo-1562036409-9dcc48472e29.jpeg')","url('./img/photo-1562967914-608f82629710.jpeg')",
+  "url('./img/photo-1565958011703-44f9829ba187.jpeg')","url('./img/photo-1561758033-d89a9ad46330 (1).jpeg')","url('./img/photo-1541832676-9b763b0239ab.jpeg')"];
+
+
+ seccionmenu=document.querySelector(".big-img");
+ 
+ i=0;
+
+
+
+ seccionatras=document.querySelector(".seccion-menu-bloque-2-atras");
+ seccionsiguiente=document.querySelector(".seccion-menu-bloque-2-siguiente");
+
+ seccionatras.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(i>0 && i<=5){
+        i--;
+    }else{
+        i=5;
+    }
+    seccionmenu.style.backgroundImage=array[i];
+    
+ })
+
+ 
+ seccionsiguiente.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(i<5){
+        i++;
+    }else{
+        i=0;
+    }
+    seccionmenu.style.backgroundImage=array[i];
+
+  
+ })
+
+
+

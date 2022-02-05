d09ada2e1f90322edd2ab8ae8e813388aa6d6b70
jeskarin36 <94501234+jeskarin36@users.noreply.github.com>
2022-02-05 02:19:00 -0400

    Add files via upload

---
+++ Script/menuarticulos.js
@@ -0,0 +1,56 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.contenedor-head',{
+    duration: 2000,
+    origin:"bottom",
+    distance: "-100px"
+})
+
+sr.reveal('.contenedor-busqueda',{
+    duration: 2000,
+    origin:"bottom",
+    distance: "50px"
+})
+sr.reveal('.publicidad',{
+    duration: 2000,
+    origin:"bottom",
+    distance: "50px"
+})
+
+sr.reveal('.seccion',{
+    duration: 2000,
+    origin:"bottom",
+    distance: "50px"
+})
+
+sr.reveal('.seccion2',{
+    duration: 2000,
+    origin:"bottom",
+    distance: "50px"
+})
+
+
+
+sr.reveal('.ani1',{
+    duration: 2000,
+    origin:"bottom",
+    distance: "50px"
+})
+
+sr.reveal('.ani2',{
+    duration: 2000,
+    origin:"bottom",
+    distance: "50px"
+})
+
+
+
+let seccion2 = document.querySelector(".seccion2")// terget class or id
+ // hear is  scroll logic
+window.addEventListener('scroll', () => {
+    window.scrollY >630 ? seccion2.classList.add("fixed") : seccion2.classList.remove("fixed")
+})
+
+
+
+ 
